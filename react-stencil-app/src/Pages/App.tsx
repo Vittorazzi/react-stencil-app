4a3ab3a7159764e442e607355fe88cd57db2d444
William Vittorazzi <wvittorazzi@gmail.com>
2022-04-11 09:38:10 -0300

    Commit inicial e pequenas mudancas

---
+++ react-stencil-app/src/Pages/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg'
+// import {theComponent} from 'the-component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='App-flex'>
+          <div>
+            <h1>Aplicação exemplo</h1>
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+          </div>
+          <div>
+            component aqui
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
